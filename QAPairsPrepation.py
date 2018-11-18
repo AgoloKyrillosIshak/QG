d4872c5d9913e13828756b022869ee41406ce97d
Kyrillos Ishak <kero.ishak@gmail.com>
2018-11-18 21:06:35 +0200

    Add files via upload
    Prepare dataset script

--- QAPairsPrepation.py
+++ QAPairsPrepation.py
@@ -1,44 +1,70 @@
-import numpy as np
-import re
-import nltk
 import json
 import csv
+import corenlp
+import os
+import copy
+import pip
+import requests
 
-def main():
-    paragraphs = []
-    questions = []
-    with open('SQUAD.csv', 'wb') as outcsv:
-        writer = csv.writer(outcsv)
-        writer.writerow(["Paragraph","Question", "Answer", "Start position","End position"])
-        with open('train-v1.1.json') as json_file:
-            data = json.load(json_file)
-            topic_list = data['data']
-            counter=0
-            for topic in topic_list:
-                for paragraph in topic['paragraphs']:
-                    #sentence = re.sub('[^A-Za-z0-9|\'|\s]+', '', paragraph['context']).lower()
-                    #tokens = nltk.word_tokenize(sentence)
-                    #paragraphs.append([vocab.encode(token) for token in tokens])
-                    index = len(paragraphs) - 1
-                    _paragraph=re.sub('[^A-Za-z0-9|\'|\s]+', '', paragraph['context'].encode('ascii', 'ignore')).lower().strip()
-                    for question in paragraph['qas']:
-                        #sentence = re.sub('[^A-Za-z0-9|\'|\s]+', '', question['question']).lower()
-                        #start_position=question['answers'][0]['answer_start']
-                        answer=re.sub('[^A-Za-z0-9|\'|\s]+', '', question['answers'][0]['text'].encode('ascii', 'ignore')).lower().strip()
-                        if counter==75201:
-                            print "heej"
-                        if answer=="" or answer=="NaN":
-                            print "ERROR A HEAD!"
-                            continue
-                        start_position = _paragraph.find(answer)
-                        end_position=start_position+len(answer)
-                        #print _paragraph[start_position:end_position] + " | "+ answer
-                        assert _paragraph[start_position:end_position]==answer
-                        quos=re.sub('[^A-Za-z0-9|\'|\s]+', '',question['question'].encode('ascii', 'ignore')).lower().strip()
-                        counter=counter+1
-                        writer.writerow([_paragraph, quos
-                                            , answer, start_position, end_position])
-        outcsv.close()
+os.environ["CORENLP_HOME"]="./CoreNLP/stanford-corenlp-full-2018-02-27/"
+Data_DIR="./Data"
 
-if __name__=="__main__":
-    main()
+
+def downloadSQuADDataset():
+    print "Downloading SQuAD dataset..."
+    response = requests.get('https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json')
+    if response.status_code>=200 and response.status_code<300:
+        file = open(Data_DIR+'/'+'train-v2.0.json','w')
+        file.write(response.content)
+        file.close()
+
+
+def extractSentences():
+    if not os.path.isdir(Data_DIR):
+        os.mkdir(Data_DIR)
+    with corenlp.CoreNLPClient(timeout=100000) as client:
+        with open(Data_DIR+"/"+'Sentences.csv', 'wb') as outcsv:
+            writer = csv.writer(outcsv)
+            writer.writerow(["Sentence", "Processed Sentence ", "Answer", "question"])
+            with open(Data_DIR+"/"+'train-v2.0.json') as json_file:
+                data = json.load(json_file)
+                topic_list = data['data']
+                counter = 0
+                dataset=[]
+                for topic in topic_list:
+                    for paragraph in topic['paragraphs']:
+                        _paragraph=paragraph['context']
+                        ann = client.annotate(_paragraph,annotators="tokenize,ssplit,pos,lemma,ner,coref".split())
+                        sentences=ann.sentence
+                        sentences_size=len(sentences)
+                        for question in paragraph['qas']:
+                            answer_position=question['answers'][0]['answer_start']
+                            answer_text=question['answers'][0]['text']
+                            len_accumelator=0
+                            cluster_map={}
+                            for sent in sentences:
+                                modifiedSent=copy.deepcopy(sent)
+                                for i in xrange(len(sent.token)):
+                                    token=sent.token[i]
+                                    if token.corefClusterID!= 0:
+                                        if not cluster_map.has_key(token.corefClusterID):
+                                            cluster_map[token.corefClusterID]=[token]
+                                        else:
+                                            if token.pos=="PRP" or token.pos=="PRP$":
+                                                for coref in cluster_map[token.corefClusterID]:
+                                                    if coref.ner!="O":
+                                                        modifiedSent.token[i].word=coref.word if token.pos=="PRP" else coref.word+"'s"
+                                                        break
+                                            cluster_map[token.corefClusterID].append(token)
+
+                                sentence_string = corenlp.to_text(sent).encode('ascii', 'ignore')
+                                if len_accumelator + len(sentence_string) > answer_position:
+                                    #dataset = (sentence_string,corenlp.to_text(modifiedSent),question['answers'][0]['text'], question['question'])
+                                    writer.writerow([sentence_string,corenlp.to_text(modifiedSent).encode('ascii', 'ignore'),question['answers'][0]['text'].encode('ascii', 'ignore'), question['question'].encode('ascii', 'ignore')])
+
+                                len_accumelator+=len(sentence_string)+1
+
+            outcsv.close()
+if __name__ == "__main__":
+    downloadSQuADDataset()
+    extractSentences()
